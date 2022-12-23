e0e3bc32bef00c09cccf70ddf2e1ccac4c2bf178
Sanket Sharma <sanketsharma132@gmail.com>
2022-12-23 12:01:36 +0530

    Add files via upload

---
+++ finalApi.js
@@ -0,0 +1,47 @@
+const { Configuration, OpenAIApi } = require("openai");
+// const fs = require("fs");
+
+let mystr;
+
+// const auth_token = require("./auth.js");
+const configuration = new Configuration({
+	apiKey: "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",
+});
+``
+const openai = new OpenAIApi(configuration);
+
+const generateText = async function () {
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt:
+			"Give a description of monkey, money, moon",
+		temperature: 0.9,
+		max_tokens: 256,``
+		top_p: 1,
+		frequency_penalty: 0,
+		presence_penalty: 0,
+	});
+	let str = response.data.choices[0].text;
+
+	str = str.replace(/\n/g, "");
+	return str;
+};
+
+generateText().then((res) => {
+	mystr = res;
+	generateImage();
+});
+
+const generateImage = async function () {
+	const response = await openai.createImage({
+		prompt: `Create an image for the details: ${mystr}`,
+		n: 1,
+		size: "512x512",
+		// response_format: "b64_json",
+	});
+	image_url = response.data.data[0].url;
+
+	// console.log(response.data.data[0].url);
+	console.log(image_url);
+	return response.data;
+};
